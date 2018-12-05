c5fbc2a0b05724f48393ef9931bbe06f9f7695f2
AdzokuMoty <32@ete.ru>
2018-12-05 21:16:43 +0300

    Начал верстать header и footer
    Пока примитив в Хэдере.

--- templates/header.php
+++ templates/header.php
@@ -4,11 +4,39 @@
     <meta charset="UTF-8">
     <title>Document</title>
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/header.css">
 </head>
 <body>
     <div class="wrapper">
         <header>
+            <menu class="head-menu-box"> 
+                <div class="head-menu-logo">
+                </div>
+                <div class="head-menu-text">
+                    <a href="womens.php">Женщинам</a>
+                    <a href="mens.php">Мужчинам</a>
+                    <a href="kids.php">Женщинам</a>
+                    <a href="novice.php">Женщинам</a>
+                    <a href="about.php">Женщинам</a>
+                </div>
+                <div class="customer-area">
+                    <div class="customer-area-box">
+                        <div class="customer-area-logo">
 
+                        </div>
+                        <div class="customer-area-button">
+                            <a href="#">Войти</a>
+                        </div>
+                    </div>
+                    <div class="customer-area-logo-basket">
+
+                    </div>
+                    <div class="customer-area-basket">
+                        <p>Корзина (0)</p>
+                    </div>
+                </div>
+            </menu>
+            <div class="head-hr-line"></div>
         </header>
         <main>
     
