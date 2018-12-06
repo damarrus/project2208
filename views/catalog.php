29314c559812a1016ec4213ad01cfa7059937592
Julia Salimyanova <juliasalim@mail.ru>
2018-12-06 22:00:31 +0300

    Добавила файлы

---
+++ views/catalog.php
@@ -0,0 +1,90 @@
+<?php require_once '../templates/header.php' ?>
+<link rel="stylesheet" href="../css/catalog.css">
+    <div class="catalog_path">
+        <a class="catalog_a" href="#">ГЛАВНАЯ</a>
+        <p>/</p>
+        <a class="catalog_a" href="#">МУЖЧИНАМ</a>
+    </div>
+    <h1 class="catalog_hello">МУЖЧИНАМ</h1>
+    <h2 class="catalog_descr">Все товары</h2>
+    <div class="lists">
+        <select name="category" id="cat_list">
+            <option value="" selected>Категория</option>
+        </select>
+        <select name="size" id="size_list">
+            <option value="" selected>Размер</option>
+        </select>
+        <select name="price" id="price_list">
+            <option value="" selected>Стоимость</option>
+        </select>
+    </div>
+    <div class="catalog_cards">
+        <div class="catalog_cards-item">
+            <img src="../images/catalog/1.jpg" alt="card_1">
+            <p id="cards-item-title">Куртка синяя</p>
+            <p id="cards-item-price">5200 руб.</p>
+        </div>
+        <div class="catalog_cards-item">
+            <img src="../images/catalog/1.jpg" alt="card_2">
+            <p id="cards-item-title">Куртка синяя</p>
+            <p id="cards-item-price">5200 руб.</p>
+        </div>
+        <div class="catalog_cards-item">
+            <img src="../images/catalog/1.jpg" alt="card_3">
+            <p id="cards-item-title">Куртка синяя</p>
+            <p id="cards-item-price">5200 руб.</p>
+        </div>
+        <div class="catalog_cards-item">
+            <img src="../images/catalog/1.jpg" alt="card_4">
+            <p id="cards-item-title">Куртка синяя</p>
+            <p id="cards-item-price">5200 руб.</p>
+        </div>
+        <div class="catalog_cards-item">
+            <img src="../images/catalog/1.jpg" alt="card_5">
+            <p id="cards-item-title">Куртка синяя</p>
+            <p id="cards-item-price">5200 руб.</p>
+        </div>
+        <div class="catalog_cards-item">
+            <img src="../images/catalog/1.jpg" alt="card_6">
+            <p id="cards-item-title">Куртка синяя</p>
+            <p id="cards-item-price">5200 руб.</p>
+        </div>
+        <div class="catalog_cards-item">
+            <img src="../images/catalog/1.jpg" alt="card_6">
+            <p id="cards-item-title">Куртка синяя</p>
+            <p id="cards-item-price">5200 руб.</p>
+        </div>
+        <div class="catalog_cards-item">
+            <img src="../images/catalog/1.jpg" alt="card_6">
+            <p id="cards-item-title">Куртка синяя</p>
+            <p id="cards-item-price">5200 руб.</p>
+        </div>
+        <div class="catalog_cards-item">
+            <img src="../images/catalog/1.jpg" alt="card_6">
+            <p id="cards-item-title">Куртка синяя</p>
+            <p id="cards-item-price">5200 руб.</p>
+        </div>
+        <div class="catalog_cards-item">
+            <img src="../images/catalog/1.jpg" alt="card_6">
+            <p id="cards-item-title">Куртка синяя</p>
+            <p id="cards-item-price">5200 руб.</p>
+        </div>
+        <div class="catalog_cards-item">
+            <img src="../images/catalog/1.jpg" alt="card_6">
+            <p id="cards-item-title">Куртка синяя</p>
+            <p id="cards-item-price">5200 руб.</p>
+        </div>
+        <div class="catalog_cards-item">
+            <img src="../images/catalog/1.jpg" alt="card_6">
+            <p id="cards-item-title">Куртка синяя</p>
+            <p id="cards-item-price">5200 руб.</p>
+        </div>
+    </div>
+    <div class="catalog_pages">
+        <div class="catalog_pages-item">1</div>
+        <div class="catalog_pages-item">2</div>
+        <div class="catalog_pages-item">3</div>
+        <div class="catalog_pages-item">4</div>
+    </div>
+
+<?php require_once '../templates/footer.php' ?>
