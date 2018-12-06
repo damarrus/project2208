c1b37f0e05caab8cfb7864fc352cb9bf628e01e6
Julia Salimyanova <juliasalim@mail.ru>
2018-12-07 01:08:04 +0300

    Изменила списки

--- js/catalog.js
+++ js/catalog.js
@@ -1,14 +1,20 @@
 $(document).ready(function(){
     console.log(123);
-    $('.catalog_list-item').click(function (event){
+    
+    var arrow = 1;
+
+    $('.catalog_list-item').on('click',function (){
 
         $(this).children('.catalog_list-item-list').slideToggle();
-        event.stopPropagation();
-        $(this).removeClass("catalog_list-item").addClass("catalog_list-item_e");
 
-        $('.catalog_list-item_e').click(function (){
-            $(this).removeClass("catalog_list-item_e").addClass("catalog_list-item");
-        });
+            if (arrow == 1) {
+            $(this).removeClass("catalog_list-item").addClass("catalog_list-item_e");
+            arrow = 2;
+            } else if (arrow == 2) {
+                $(this).removeClass("catalog_list-item_e").addClass("catalog_list-item");
+                arrow = 1;
+            }
 
     });
-})
+
+});
