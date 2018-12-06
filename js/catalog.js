54c4281f6596605cef64bd2342542739f66ed7a8
Julia Salimyanova <juliasalim@mail.ru>
2018-12-07 00:41:34 +0300

    Изменила выпадающие списки

--- js/catalog.js
+++ js/catalog.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
     console.log(123);
     $('.catalog_list-item').click(function (event){
+
         $(this).children('.catalog_list-item-list').slideToggle();
         event.stopPropagation();
+        $(this).removeClass("catalog_list-item").addClass("catalog_list-item_e");
+
+        $('.catalog_list-item_e').click(function (){
+            $(this).removeClass("catalog_list-item_e").addClass("catalog_list-item");
+        });
+
     });
 })
