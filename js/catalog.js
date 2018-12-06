cd762083816e30db00df0054d9e04a3746d9b9bd
Julia Salimyanova <juliasalim@mail.ru>
2018-12-07 01:09:22 +0300

    Update catalog.js

--- js/catalog.js
+++ js/catalog.js
@@ -8,8 +8,8 @@ $(document).ready(function(){
         $(this).children('.catalog_list-item-list').slideToggle();
 
             if (arrow == 1) {
-            $(this).removeClass("catalog_list-item").addClass("catalog_list-item_e");
-            arrow = 2;
+                $(this).removeClass("catalog_list-item").addClass("catalog_list-item_e");
+                arrow = 2;
             } else if (arrow == 2) {
                 $(this).removeClass("catalog_list-item_e").addClass("catalog_list-item");
                 arrow = 1;
