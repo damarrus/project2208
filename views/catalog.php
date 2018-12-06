67f143ce8a4ecdd5bac2eb135490edbca512a1dc
Julia Salimyanova <juliasalim@mail.ru>
2018-12-06 23:41:33 +0300

    Изменила стили

--- views/catalog.php
+++ views/catalog.php
@@ -10,15 +10,31 @@
     <div class="catalog_list">
         <div class="catalog_list-item">
             <p>Категория</p>
-            <div class="catalog_list-item-list"></div>
+            <div class="catalog_list-item-list">
+                <p>Мужчинам</p>
+                <p>Женщинам</p>
+                <p>Детям</p>
+            </div>
         </div>
         <div class="catalog_list-item">
             <p>Размер</p>
-            <div class="catalog_list-item-list"></div>
+            <div class="catalog_list-item-list">
+                <p>XS</p>
+                <p>S</p>
+                <p>M</p>
+                <p>L</p>
+                <p>XL</p>
+                <p>XXL</p>
+            </div>
         </div>
         <div class="catalog_list-item">
             <p>Стоимость</p>
-            <div class="catalog_list-item-list"></div>
+            <div class="catalog_list-item-list">
+                <p>0-1000 руб.</p>
+                <p>1000-3000 руб.</p>
+                <p>3000-6000 руб.</p>
+                <p>6000-20000 руб.</p>
+            </div>
         </div>
     </div>
     <div class="catalog_cards">
