f629954c569c2f0cf98d81d49e6edbb43c58efc3
David Martirosyan <itsdavid71@gmail.com>
2018-12-06 23:04:52 +0300

    Выполнил верстку cart.php

--- views/cart.php
+++ views/cart.php
@@ -6,55 +6,163 @@
         <h1 class="main-title">Ваша корзина</h1>
         <h2 class="main-description">Товары резервируются на ограниченное время</h2>
     </div>
-    
-    <table class="main-cart">
-        <tr class="main-cart-column">
-            <th class="main-cart-column-title">Фото</th>
-            <th class="main-cart-column-title">Наименование</th>
-            <th class="main-cart-column-title">Размер</th>
-            <th class="main-cart-column-title">Количество</th>
-            <th class="main-cart-column-title">Стоимость</th>
-            <th class="main-cart-column-title">Удалить</th>
-        </tr>
-        <tr>
-            <div class="hr"></div>
-        </tr>
-        <tr>
-            <td class="main-cart-pruduct">
-                <div class="main-cart-pruduct-block">
-                    <div class="main-cart-product-block-img"></div>
-                </div>
-            </td>
 
-            <td>
-                <div class="main-cart-product-block-name">
-                    <p>Куртка синяя</p>
-                    <p>арт. 123441</p>
-                </div>
-            </td>
-            <td>
-                <div class="main-cart-product-block-name">
-                    <p>Куртка синяя</p>
-                </div>
-            </td>
-            <td>
-                <div class="main-cart-product-block-name">
-                    <p>Куртка синяя</p>
-                </div>
-            </td>
-            <td>
-                <div class="main-cart-product-block-name">
-                    <p>Куртка синяя</p>
-                </div>
-            </td>
-            <td>
-                <div class="main-cart-product-block-name">
-                    <p>Куртка синяя</p>
-                </div>
-            </td>
-            
+    <table>
+        <col width="0">
+        <col width="820">
+        <tr>
+            <td class="main-cart-column-title">Фото</th>
+            <td class="main-cart-column-title">Наименование</th>
+            <td class="main-cart-column-title">Размер</th>
+            <td class="main-cart-column-title">Количество</th>
+            <td class="main-cart-column-title">Стоимость</th>
+            <td class="main-cart-column-title">Удалить</th>
         </tr>
+        <tr>
+            <td>
+                <div class="main-cart-product-block-img"></div>
+            </td>
+            <td>
+                <div class="main-cart-product-block-name">
+                    <p class="main-cart-product-block-name-title">Куртка синяя</p>
+                    <p class="main-cart-product-block-name-articul">арт. 123441</p>
+                </div>
+            </td>
+            <td class="tac">39</td>
+            <td class="tac">1</td>
+            <td class="tac">3800 руб.</td>
+            <td>
+                <div class="main-cart-product-block-delete"></div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <div class="main-cart-product-block-img"></div>
+            </td>
+            <td>
+                <div class="main-cart-product-block-name">
+                    <p class="main-cart-product-block-name-title">Куртка синяя</p>
+                    <p class="main-cart-product-block-name-articul">арт. 123441</p>
+                </div>
+            </td>
+            <td class="tac">39</td>
+            <td class="tac">1</td>
+            <td class="tac">3800 руб.</td>
+            <td>
+                <div class="main-cart-product-block-delete"></div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <div class="main-cart-product-block-img"></div>
+            </td>
+            <td>
+                <div class="main-cart-product-block-name">
+                    <p class="main-cart-product-block-name-title">Куртка синяя</p>
+                    <p class="main-cart-product-block-name-articul">арт. 123441</p>
+                </div>
+            </td>
+            <td class="tac">39</td>
+            <td class="tac">1</td>
+            <td class="tac">3800 руб.</td>
+            <td>
+                <div class="main-cart-product-block-delete"></div>
+            </td>
+        </tr>                
     </table>
+    <div class="main-cart-total">
+        <div class="main-cart-total-text">Итого: <span class="main-cart-total-text-sum">12500 руб.</span></div>
+    </div>
+
+    <div class="orange">
+        <div class="main-cart-orange">
+            <div class="main-cart-orange-block"></div>
+            <div class="main-cart-orange-block"></div>
+            <div class="main-cart-orange-block"></div>
+        </div>
+    </div>
+
+    <div class="main-cart">
+        <h3 class="address-title">Адрес доставки</h3>
+        <h2 class="main-description">Все поля обязательны для заполнения</h2>
+        <div class="main-card-address-flex">
+            <form class="main-cart-address-form">
+                <p class="main-cart-address-form-text">Выберите вариант доставки</p>
+                <select class="select-style" name="" id="">
+                    <option value="">Курьерская служба - 500р</option>
+                    <option value="">Курьерская служба - 500р</option>
+                    <option value="">Курьерская служба - 500р</option>
+                    <option value="">Курьерская служба - 500р</option>
+                </select>
+                <div class="main-cart-address-form-line">
+                    <div class="main-cart-address-form-line-block">
+                        <p class="main-cart-address-form-text">Имя</p>
+                        <input class="input-style main-cart-address-form-line-input" type="text">
+                    </div>
+                    <div class="main-cart-address-form-line-block">
+                        <p class="main-cart-address-form-text">Фамилия</p>
+                        <input class="input-style main-cart-address-form-line-input" type="text">
+                    </div>    
+                </div>
+                <div class="main-cart-address-form-line">
+                    <div class="main-cart-address-form-line-block">
+                        <p class="main-cart-address-form-text">Адрес</p>
+                        <input class="input-style main-cart-address-form-line-input-long" type="text">
+                    </div>
+                </div>
+                <div class="main-cart-address-form-line">
+                    <div class="main-cart-address-form-line-block">
+                        <p class="main-cart-address-form-text">Город</p>
+                        <input class="input-style main-cart-address-form-line-input" type="text">
+                    </div>
+                    <div class="main-cart-address-form-line-block">
+                        <p class="main-cart-address-form-text">Индекс</p>
+                        <input class="input-style main-cart-address-form-line-input" type="text">
+                    </div>    
+                </div>
+                <div class="main-cart-address-form-line">
+                    <div class="main-cart-address-form-line-block">
+                        <p class="main-cart-address-form-text">Телефон</p>
+                        <input class="input-style main-cart-address-form-line-input" type="text">
+                    </div>
+                    <div class="main-cart-address-form-line-block">
+                        <p class="main-cart-address-form-text">E-Mail</p>
+                        <input class="input-style main-cart-address-form-line-input" type="text">
+                    </div>    
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="orange">
+        <div class="main-cart-orange">
+            <div class="main-cart-orange-block"></div>
+            <div class="main-cart-orange-block"></div>
+            <div class="main-cart-orange-block"></div>
+        </div>
+    </div>
+
+    <div class="main-cart">
+        <h3 class="address-title">Варианты оплаты</h3>
+        <h2 class="main-description">Все поля обязательны для заполнения</h2>
+        <div class="main-cart-pay">
+            <div class="main-cart-pay-sum">
+                <p>Стоимость: 12500 руб.</p>
+                <p>Доставка: 500 руб.</p>
+                <p class="main-cart-pay-sum-total">Итого: 13000 руб.</p>
+            </div>
+            <div class="main-card-address-flex">
+            <form class="main-cart-address-form">
+                <p class="main-cart-address-form-text">Выберите вариант доставки</p>
+                <select class="select-style" name="" id="">
+                    <option value="">Банковская карта</option>
+                    <option value="">Яндекс.Деньги</option>
+                    <option value="">WebMoney</option>
+                </select>
+                <input class="main-cart-pay-button" type="submit" value="Заказать">
+            </form>
+        </div>
+    </div>
+
 </div>
 
 
