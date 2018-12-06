5adbf1885240298114d96461baba3d8e76583ddc
Julia Salimyanova <juliasalim@mail.ru>
2018-12-06 23:23:09 +0300

    Внесла изменения

--- js/catalog.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    console.log(123);
-    $('.catalog_list-item').click(function (event){
-        $(this).children('.catalog_list-item-list').slideToggle();
-        event.stopPropagation();
-    });
-})
