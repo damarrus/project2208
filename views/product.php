60eba4fff121bd37b23a805a7a20c96c68f6e948
Martynro <martynro@gmail.com>
2018-12-07 19:29:54 +0300

    Верстка страницы товара

---
+++ views/product.php
@@ -0,0 +1,33 @@
+<?php require_once '../templates/header.php' ?>
+<link rel="stylesheet" href="../css/product.css">
+
+
+<div class="product-img">
+</div>
+
+<div class="product-description">
+    <h1>Кеды с полоской</h1>
+        <h4>Артикул 380954</h4>
+            <br>
+                <h3><i>4500 руб.</i><h3>
+                    <br>
+                        <p>Отличные кеды из водонепроницаемого материала. Отлично подходят для любой погоды. Приятно сидят на ноге, стильные и комфортные</p>
+                            <br>
+</div>
+
+<div class="product-size">
+    <h3>РАЗМЕР</h3>
+        <div class="sizes">
+            <div class="size">38</div>
+            <div class="size">39</div>
+            <div class="size">40</div>
+            <div class="size">41</div>
+            <div class="size">42</div>
+        </div>
+</div>
+
+<div class="prodcut-button">
+    <a href="#">Добавить в корзину</a>
+</div>
+
+<?php require_once '../templates/footer.php' ?>
