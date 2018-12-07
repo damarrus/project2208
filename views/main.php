2c878a4d17986bfc9c2ba43120d1ceb70571520c
EgorSWEB <silvestrov2002@mail.ru>
2018-12-07 19:47:02 +0300

    main

---
+++ views/main.php
@@ -0,0 +1,64 @@
+<?php require_once '../templates/header.php' ?>
+<link rel="stylesheet" href="../css/main.css">
+
+<div class="main-center">
+    <h1>НОВЫЕ ПОСТУПЛЕНИЯ ВЕСНЫ</h1>
+    <p><i>Мы подготовили для Вас лучшие предложения сезона</i></p>
+    <p><button class="main-button">ПОСМОТРЕТЬ НОВИНКИ</button></p>
+    <div class="main-b">
+        <div class="main-b-flex1">
+            <div class="b1 img1" >
+                <div>
+                    ДЖИНСОВЫЕ<BR> 
+                    КУРТКИ<BR>
+                    <i>NEW ARRIVAL</i>
+                </div>
+            </div>
+            <div class="b2 img7"></div>
+        </div>
+        <div class="main-b-flex2">
+            <div class="b2 light-gray"><i>Каждый сезон мы подготавливаем для Вас исключительно лучшую модну одежду. Следите за нашими новинками</i></div>
+            <div class="b2 img3"></div>
+            <div class="b2 img2">ДЖИНСЫ <BR> от 3200 руб.</div>
+            <div class="b2 light-gray"><i>Самые низкие цены в Москве. <BR> Нашли дешевле? Вернем разницу.</i></div>
+            <div class="b2 gray">АКСЕССУАРЫ</div>
+            <div class="b2 img8">
+                <div>
+                    СПОРТИВНАЯ<BR> 
+                    ОДЕЖДА<BR>
+                    <i>от 590 руб.</i>
+                </div>
+            </div>
+        </div>
+        <div class="main-b-flex1">
+            <div class="b2 gray">
+                <div>
+                    ЭЛЕГАНТНАЯ ОБУВЬ <BR> 
+                    <i>БОТИНКИ, КРОССОВКИ</i>
+                </div>
+            </div>
+            <div class="b1 img4">
+                <div>
+                    ДЕТСКАЯ ОДЕЖДА <BR> 
+                    <i>NEW ARRIVAL</i>
+                </div>
+            </div>
+        </div>
+    </div>
+    <h1>БУДЬ ВСЕГДА В КУРСЕ ВЫГОДНЫХ ПРЕДЛОЖЕНИЙ</h1>
+    <p><i>подписывайся, следи за новинками и выгодными предложениями</i></p>
+    <p class="red_color"><i>Некорректный email. Попробуйте еще раз.</i></p>
+    <form action="" class="podpiska">
+        <input type="email" name="email" placeholder="email" class="input-email">
+        <button class="button-email">Подписаться</button>
+    </form>        
+    </div>  
+
+
+
+</div>
+
+
+<?php require_once '../templates/footer.php' ?>
+
+
