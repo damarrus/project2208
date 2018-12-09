927b5526f1a05c4508a2871d8698642495dcb3bb
AdzokuMoty <32@ete.ru>
2018-12-09 19:03:13 +0300

    upload Collection.php
    upload Collection.php

---
+++ models/Collection.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once '../db.php';
+
+class Collection
+{
+    public $id;
+    public $title;
+    public $description;
+
+    public function __construct($id)
+    {
+        global $mysqli;
+
+        $query = "SELECT collection_id, title, description FROM collections 
+            WHERE collection_id = $id";
+        $result = $mysqli->query($query);
+
+        $data = $result->fetch_assoc();
+
+        $this->id = $data['collection_id'];
+        $this->title = $data['title'];
+        $this->description = $data['description'];
+    }
+
+    public static function getAll()
+    {
+        global $mysqli;
+
+        $query = "SELECT collection_id FROM collections";
+        $result = $mysqli->query($query);
+        
+        $collections = [];
+        while ($Collection_data = $result->fetch_assoc()) {
+            $collections[] = new Collection($Collection_data['collection_id']);
+        }
+
+        return $collections;
+    }
+
+
+}
+    // $Collection = new Collection(2);
+    // var_dump($Collection->title);
+
+    // $collections = Collection::getAll();
+    // var_dump($collections);
+
+    // foreach ($collections as $collection) {
+    //       echo '<h1>'.$collection->title.'</h1>';
+    // }
