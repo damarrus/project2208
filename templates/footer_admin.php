fb2d80c38a1e6ba3601dc48dff115a6918c5c17e
Julia Salimyanova <juliasalim@mail.ru>
2018-12-10 21:34:43 +0300

    Сверстала админку

---
+++ templates/footer_admin.php
@@ -0,0 +1,11 @@
+    </main>
+    <footer>
+    </footer> 
+
+    <script src="../lib/jquery-3.3.1.min.js"></script>
+    <script src="../lib/popper.min.js"></script>
+    <script src="../lib/bootstrap.min.js"></script>
+    <!-- <script src="main.js"></script> -->
+    </div>
+</body>
+</html>
