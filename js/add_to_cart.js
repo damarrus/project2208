a116a13d7d6c6af01c03c59e9dd0c75855e72ee2
David <>
2018-12-12 21:29:40 +0300

    Update add_to_cart.js

---
+++ js/add_to_cart.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    $('#product-button').click(function() {
+        var title = $('.product-title').text();
+        var price = $('.product-price').text();
+
+        console.log(price);
+        var data = {
+            title: title,
+            price: price
+        }
+
+        data = JSON.stringify(data);
+        console.log(data);
+        
+        // var xhr = new XMLHttpRequest();
+        // xhr.open('POST', 'add_to_cart.php', true);
+        // xhr.setRequestHeader('Content-type', 'application/json');
+        // xhr.send(data);
+
+        var xhr = $.ajax({
+            type: "POST",
+            url: 'add_to_cart.php',
+            data: data,
+            success: true,
+            dataType: 'Content-type'
+          });
+
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState != 4) {
+                return;
+            }
+            
+            var response = JSON.parse(xhr.responseText);
+            console.log(response); 
+
+        }
+    });
+});
