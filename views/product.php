7e147c5078b48a5f4131eb15974be7b4a510da52
David <itsdavid71@gmail.com>
2018-12-23 02:10:57 +0300

    Подредактировал view/product.php

--- views/product.php
+++ views/product.php
@@ -1,54 +1,39 @@
-<?php require_once '../templates/header.php';
-      require_once '../models/Product.php';
-      require_once '../models/Size.php';
-?>
-   
-   
-            
+<?php require_once '../templates/header.php';?>      
 <link rel="stylesheet" href="../css/product.css">
 
 
-<div class="product-img">
-</div>
+<div class="product-img"></div>
 
 <div class="product-description">
     <h1 class="product-title">
         <?php 
-            $product = new Product($_GET['product_id']);
             echo $product->title;
         ?>  
     </h1>
-        <h4>Артикул 380954</h4>
-            <br>
-                <h3><i class="product-price">
-                        <?php 
-                            $product = new Product($_GET['product_id']);
-                            echo $product->price . ' руб.';
-                        ?> 
-                    </i>
-                <h3>
-                    <br>
-                        <p>Отличные кеды из водонепроницаемого материала. Отлично подходят для любой погоды. Приятно сидят на ноге, стильные и комфортные</p>
-                            <br>
+        <h4 class="pb">Артикул 380954</h4>
+        <h3 class="product-price pb">
+            <?php 
+                echo $product->price . ' руб.';
+            ?>
+        </h3>
+        <p class="pb">Отличные кеды из водонепроницаемого материала. Отлично подходят для любой погоды. Приятно сидят на ноге, стильные и комфортные</p>
 </div>
 
-<div class="product-size">
+<div class="product-size pb">
     <h3>РАЗМЕР</h3>
-        <div class="sizes">
-        
-        <?php 
-            $sizes = Size::getAllByProduct($_GET['product_id']);
-            foreach ($sizes as $size) {
-                echo '<div class="sizes-item"><p>'.$size->value.'</p></div>';
-                }
-        ?>
-       
-            <!-- <div class="size" value="38">38</div>
-            <div class="size" value="39">39</div>
-            <div class="size" value="40">40</div>
-            <div class="size" value="41">41</div>
-            <div class="size" value="42">42</div> -->
-        </div>
+    <div class="sizes">
+    <?php 
+        foreach ($sizes as $size) {
+            echo '<div class="sizes-item"><p>'.$size->value.'</p></div>';
+            }
+    ?>
+    
+        <!-- <div class="size" value="38">38</div>
+        <div class="size" value="39">39</div>
+        <div class="size" value="40">40</div>
+        <div class="size" value="41">41</div>
+        <div class="size" value="42">42</div> -->
+    </div>
 </div>
 <div class="product-button" id="product-button">
     <span>Добавить в корзину</span>
