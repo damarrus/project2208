a9d1c3bb637f4906fad9a823f5749929ee7e8a44
David <>
2019-01-09 19:28:53 +0300

    Update

--- controllers/product_delete.php
+++ controllers/product_delete.php
@@ -2,11 +2,9 @@
 
 require_once '../models/Product.php';
 
-// $product = Product::delete($_POST['product_id']);
 $product = new Product($_POST['product_id']);
 
-$product->delete($_POST['product_id']);
-// var_dump($mysqli->error);
-// die;
+$product->delete();
+
 
 header('Location: product_delete_form.php?success=1&product_id='.$_POST['product_id']);
